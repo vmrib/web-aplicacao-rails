5e9d1cc794eeb293ea5f98f1a02a9e30b4e90e83
Vinicius M Ribeiro <vmr20@inf.ufpr.br>
2023-06-22 14:55:20 -0300

    simple tests

--- test/models/documento_test.rb
+++ test/models/documento_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class DocumentoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid document" do
+    valid = documentos(:valid)
+    assert valid.save
+  end
+
+  test "invalid document" do
+    invalid = documentos(:rgInvalidChar)
+    assert_not invalid.save
+
+    invalid = documentos(:cpfInvalidChar)
+    assert_not invalid.save
+
+    invalid = documentos(:noRg)
+    assert_not invalid.save
+
+    invalid = documentos(:noCpf)
+    assert_not invalid.save
+  end
+
+  test "uniqueness document" do
+    valid = documentos(:valid)
+    assert valid.save
+
+    sameDocument = documentos(:valid)
+    assert_not sameDocument.save
+  end
 end
--- test/models/estado_test.rb
+++ test/models/estado_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class EstadoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid state" do
+    valid = estados(:valid)
+    assert valid.save?
+  end
+
+  test "invalid state" do
+    invalid = estados(:tooLong)
+    assert_not invalid.save
+
+    invalid = estados(:invalidChar)
+    assert_not invalid.save
+
+    invalid = estados(:noSigla)
+    assert_not invalid.save
+  end
+
+  test "uniqueness state" do
+    valid = estados(:valid)
+    assert valid.save
+
+    sameState = estados(:valid)
+    assert_not sameState.save
+  end
 end
